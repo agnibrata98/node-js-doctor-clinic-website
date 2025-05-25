be0f34f3e58c54421b615f0ee0f10c64dc2b517c
agnibrata98 <agnibratashee@gmail.com>
2025-05-25 13:06:10 +0530

    making dynamic the repeated sections

--- app/controller/ServiceController.js
+++ app/controller/ServiceController.js
@@ -1,8 +1,29 @@
 class ServiceController {
     async servicePage(req, res) {
+        const testimonial = [
+            {
+                name: 'John Doe',
+                proffession: 'Web Developer',
+                comment: 'Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.',
+                image: '/img/testimonial-1.jpg'
+            },
+            {
+                name: 'Jane Doe',
+                proffession: 'Web Developer',
+                comment: 'Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.',
+                image: '/img/testimonial-2.jpg'
+            },
+            {
+                name: 'John Doe',
+                proffession: 'Web Developer',
+                comment: 'Clita clita tempor justo dolor ipsum amet kasd amet duo justo duo duo labore sed sed. Magna ut diam sit et amet stet eos sed clita erat magna elitr erat sit sit erat at rebum justo sea clita.',
+                image: '/img/testimonial-3.jpg'
+            }
+        ]
         try {
             res.render('service', {
-                title: 'Service page'
+                title: 'Service page',
+                testimonial: testimonial
             });
         } catch (error) {
             console.log(error);
--- app/controller/TeamController.js
+++ app/controller/TeamController.js
@@ -1,8 +1,51 @@
 class TeamController {
     async teamPage(req, res) {
+        const team = [
+            {
+                name: 'John Doe',
+                department: 'Web Developer',
+                image: '/img/team-1.jpg'
+            },
+            {
+                name: 'Jane Doe',
+                department: 'Web Developer',
+                image: '/img/team-2.jpg'
+            },
+            {
+                name: 'John Doe',
+                department: 'Web Developer',
+                image: '/img/team-3.jpg'
+            },
+            {
+                name: 'John Doe',
+                department: 'Web Developer',
+                image: '/img/team-4.jpg'
+            },
+            {
+                name: 'John Doe',
+                department: 'Web Developer',
+                image: '/img/team-2.jpg'
+            },
+            {
+                name: 'John Doe',
+                department: 'Web Developer',
+                image: '/img/team-3.jpg'
+            },
+            {
+                name: 'John Doe',
+                department: 'Web Developer',
+                image: '/img/team-4.jpg'
+            },
+            {
+                name: 'John Doe',
+                department: 'Web Developer',
+                image: '/img/team-1.jpg'
+            }
+        ]
         try {
             res.render('team', {
-                title: 'Team page'
+                title: 'Team page',
+                team: team
             });
         } catch (error) {
             console.log(error);
